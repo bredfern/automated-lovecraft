1ed86f76f4750f238ce9821c4962a05e80c8fede
Brian Redfern <brianwredfern@gmail.com>
2015-11-11 00:06:40 +0000

    added a second example

--- generate_sample.sh
+++ generate_sample.sh
@@ -1,1 +1,1 @@
-th sample.lua -temperature 0.7 -primetext "The Great Old Ones " -length 600000 cv/lm_lstm_epoch50.00_1.2692.t7 > automated_lovecraft.txt
+th sample.lua -temperature 0.5 -primetext "The Great Old Ones " -length 600000 cv/lm_lstm_epoch50.00_1.2692.t7 > automated_lovecraft2.txt
