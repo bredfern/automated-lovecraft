1f7fb7a60d19aa57892ec5c0b70d33c8e300553d
Brian Redfern <brianwredfern@gmail.com>
2016-10-28 01:15:42 -0700

    updated models

--- generate_sample.sh
+++ generate_sample.sh
@@ -1,1 +1,1 @@
-th sample.lua -temperature 0.5 -primetext "The Great Old Ones " -length 600000 cv/lm_lstm_epoch50.00_1.2692.t7 > automated_lovecraft2.txt
+th sample.lua -temperature 0.5 -primetext "The Great Old Ones " -length 600000 cv/lm_lstm_epoch49.50_1.2683.t7 > automated_lovecraft2.txt
